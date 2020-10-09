f373eed3e1b015fd0c781db83b70e31524fa178e
Scott <Scott@SCOTT-PC>
2020-10-09 06:31:47 -0700

    connection string

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,5 +7,8 @@ module.exports = {
   "port": process.env.MIGRATION_DATABASE_PORT,
   "database": process.env.MIGRATION_DATABASE_NAME,
   "username": process.env.MIGRATION_DATABASE_USER,
-  "password": process.env.MIGRATION_DATABASE_PASS
+  "password": process.env.MIGRATION_DATABASE_PASS,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL
 }
